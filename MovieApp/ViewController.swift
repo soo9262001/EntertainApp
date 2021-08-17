bbf6c52bc7a7aaed874cd46413b34dbbc9581644
soo <soo9262001@naver.com>
2021-08-17 21:17:02 +0900

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by kms on 2021/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
